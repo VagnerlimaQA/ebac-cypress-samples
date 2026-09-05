ebf0b4e426f5fada0bf4e0695213b48b19f3d3ea
Vagner Lima <Vagner_lima97@hotmail.com>
2026-09-05 12:53:51 -0300

    configuração workflow 2

--- cypress/e2e/2-advanced-examples/misc.cy.js
+++ cypress/e2e/2-advanced-examples/misc.cy.js
@@ -5,12 +5,12 @@ context('Misc', () => {
     cy.visit('https://example.cypress.io/commands/misc')
   })
 
-  it('.end() - end the command chain', () => {
+  it.skip('.end() - end the command chain', () => {
     // https://on.cypress.io/end
 
     // cy.end is useful when you want to end a chain of commands
     // and force Cypress to re-query from the root element
-    cy.get('.misc-table').within(() => {
+      cy.get('.misc-table').within(() => {
       // ends the current chain and yields null
       cy.contains('Cheryl').click().end()
 
